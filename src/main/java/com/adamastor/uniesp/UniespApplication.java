6997b1283873ea701e14ab1f31f9d4ead00f58f8
adamastorfranca <adamastorfranca@hotmail.com>
2023-02-28 22:56:55 -0300

    feat: desenvolvimento inicial do projeto no back end com spring

---
+++ src/main/java/com/adamastor/uniesp/UniespApplication.java
@@ -0,0 +1,13 @@
+package com.adamastor.uniesp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniespApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniespApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adamastor/uniesp/UniespApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adamastor.uniesp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniespApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
